971ea77ce6af866a7c5f58c81ad96f6ed48da04a
NearMaick <maick_a_s@msn.com>
2021-07-01 17:56:42 -0300

    chore(mobile): useState explanation

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,5 @@
 import React, { useState } from 'react';
-import {
-  SafeAreaView,
-  Text,
-  TextInput,
-  FlatList,
-  ScrollView,
-} from 'react-native';
+import { SafeAreaView, Text, TextInput, FlatList } from 'react-native';
 import { styles } from './styles';
 
 import { Button } from '../../components/Button';
@@ -28,6 +22,7 @@ export function Home() {
         placeholder="New Skill"
         placeholderTextColor="#555"
         onChangeText={setNewSkill}
+        // onChangeText={text => setNewSkill(text)}
       />
 
       <Button onPress={handleAddNewSkill} />
